aaf731e405f0588ce639d58ca7a66031c4fa1cf2
stephanie-warmenhoven <steph_war@yahoo.ca>
2022-01-28 11:33:25 -0800

    notes on creating a new branch

--- git/git_branching_and_merging/learn_branching.js
+++ git/git_branching_and_merging/learn_branching.js
@@ -1,2 +1,20 @@
 //This file is created to show a different commit history on the learn_branching branch
 // than the master branch
+
+//To check your branches:
+// $ git branch
+
+//Before creating a new branch, make sure that you have saved/commited
+//everything on your current working branch 
+
+//To create a new branch
+// $ git branch <new-branch-name>
+
+//To checkout to the new branch:
+// $ git checkout <new-branch-name>
+
+//To create a new branch and switch to it at the same time:
+// $ git checkout -b <new-branch-name>
+
+
+
