118d882648419d92633166c35813e46c703bd922
stephanie-warmenhoven <steph_war@yahoo.ca>
2022-03-04 09:18:12 -0800

    rails 7 fix turbo form submission status

--- rails_project/awesome_answers/app/controllers/contacts_controller.rb
+++ rails_project/awesome_answers/app/controllers/contacts_controller.rb
@@ -6,6 +6,12 @@ class ContactsController < ApplicationController
     @name=params[:name]
     @email=params[:email]
     @message=params[:message]
-    # render status: 303
+    render status: 303
+    #need the above render because following a form submission will give a status 200, 
+    #which normally indicates a GET method and not a POST method. 
+    #Rails 7 ships with Turbo. It expects a form submission to be followed by a 303 code redirect, 
+    #one exception to this being when you need to render validation errors.
+    #See docs here: https://guides.rubyonrails.org/layouts_and_rendering.html
+    #More info: https://turbo.hotwired.dev/handbook/introduction
   end
 end
