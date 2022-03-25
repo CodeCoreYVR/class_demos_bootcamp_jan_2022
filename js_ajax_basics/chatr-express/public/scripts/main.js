d889e3e9f4ce4eba5855d8dd61288bf26fea88b9
stephanie-warmenhoven <steph_war@yahoo.ca>
2022-03-25 13:03:13 -0700

    js ajax chatr message ajax requests for index, create, and delete

--- js_ajax_basics/chatr-express/public/scripts/main.js
+++ js_ajax_basics/chatr-express/public/scripts/main.js
@@ -22,45 +22,124 @@ fetch("http://localhost:3434/messages")
 .then(console.table)
 
 //list of messages
-const loadMessages = () => {
-    fetch("/messages")
-    .then(res => res.json())
-    .then(messages => {
-        const messagesContainer = document.querySelector("#messages");
-        let messagesHTML = "";
-        messages.forEach(message => {
-            messagesHTML += `
-            <li>
-                ${message.body}
-                <i  data-id=${message.id} class="delete-link">x</i>
-            </li>
-            `;
-            messagesContainer.innerHTML = messagesHTML;
+// const loadMessages = () => {
+//     fetch("/messages")
+//     .then(res => res.json())
+//     .then(messages => {
+//         const messagesContainer = document.querySelector("#messages");
+//         let messagesHTML = "";
+//         messages.forEach(message => {
+//             messagesHTML += `
+//             <li>
+//                 ${message.body}
+//                 <i  data-id=${message.id} class="delete-link">x</i>
+//             </li>
+//             `;
+//             messagesContainer.innerHTML = messagesHTML;
+//         })
+//     })
+// }
+
+// //refresh list intermittently
+// const refreshIntervalMsg = 3000;
+// document.addEventListener("DOMContentLoaded", () => {
+//     loadMessages();
+//     setInterval(loadMessages, refreshIntervalMsg)
+// });
+
+// //POST AJAX req to create hard coded message
+// //req Form
+// const fd = new FormData();
+// fd.set("body", "Hello World!")
+// fetch("/messages", {
+//     method: "POST",
+//     body: fd
+// });
+
+// //POST AJAX to create message throught headers and hard coded data
+// fetch("/messages", {
+//     method: "POST",
+//     headers: { "Content-type": "application/json" },
+//     body: JSON.stringify({ body: "Goodbye World"})
+// })
+
+//All our requests to messages
+const Message = {
+    index(){
+        return fetch("http://localhost:3434/messages")
+        .then(response => response.json())
+    },
+    create(params){
+        return fetch('/messages', {
+            method: "POST",
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify(params)
         })
-    })
+    },
+    delete(id){
+        return fetch(`/messages/${id}`, {
+            method: "DELETE"
+        }) 
+    }
 }
 
-//refresh list intermittently
-const refreshIntervalMsg = 3000;
-document.addEventListener("DOMContentLoaded", () => {
-    loadMessages();
-    setInterval(loadMessages, refreshIntervalMsg)
-});
+document.addEventListener('DOMContentLoaded', () => {
+    const messagesUL = document.querySelector('#messages')
+    const messageForm = document.querySelector('#new-message')
 
-//POST AJAX req to create hard coded message
-//req Form
-const fd = new FormData();
-fd.set("body", "Hello World!")
-fetch("/messages", {
-    method: "POST",
-    body: fd
-});
+    const refreshMessages = () => {
+        Message.index()
+        .then(messages => {
+            messagesUL.innerHTML = messages.map(message => {
+                return `
+                <li>
+                    <strong>#${message.id}</strong>
+                    ${message.body}
+                    </br>
+                    <button data-id=${message.id} class="delete-button">Delete</button>
+                </li>
+                `;
+            }).join('')
+        })
+    }
+    setInterval(refreshMessages, 3000)
 
-//POST AJAX to create message throught headers and hard coded data
-fetch("/messages", {
-    method: "POST",
-    headers: { "Content-type": "application/json" },
-    body: JSON.stringify({ body: "Goodbye World"})
+    messageForm.addEventListener('submit', event => {
+        event.preventDefault()
+
+        const { currentTarget } = event; //the form element
+
+        //Use FormData to create an object representation
+        //of key value pairs of the form that we pass as an argument
+        //to the constructor
+        const formData = new FormData(currentTarget)
+
+        //formData.get returns the value associated with the given key
+        //from within the FormData object
+
+        Message.create({ body: formData.get("body")})
+        .then(() => {
+            console.log("Message Created!")
+            refreshMessages()
+            currentTarget.reset() //reset empties the form inputs
+        })
+    })
+
+    messagesUL.addEventListener('click', event => {
+        const { target } = event //the element that triggered the event
+
+        if (target.matches('.delete-button')){
+            Message.delete(target.dataset.id)
+            .then(() => {
+                console.log("Message deleted!")
+                refreshMessages()
+            })
+        }
+    })
+   
 })
+
+
+
 
 
