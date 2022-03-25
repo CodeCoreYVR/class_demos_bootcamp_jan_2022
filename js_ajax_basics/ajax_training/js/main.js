67240539bb076311d9c2e27aa69367f74fb1e249
stephanie-warmenhoven <steph_war@yahoo.ca>
2022-03-25 10:11:06 -0700

    js ajax initial simple ajax demo

---
+++ js_ajax_basics/ajax_training/js/main.js
@@ -0,0 +1,8 @@
+//AJAX - Asynchronous Javascript and XML
+
+//AJAX is a set of techniques to fetch data from a server asynchronously
+//without interfering with the display and behaviour of the existing page in the browser
+//What this means is that our page loads while in the background AJAX fetches data
+//from the server and makes it available for our use
+
+
