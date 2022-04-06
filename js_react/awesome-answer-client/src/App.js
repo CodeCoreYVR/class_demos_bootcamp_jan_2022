609c559a382998f5291fe29b31b3c2724d1a6a35
stephanie-warmenhoven <steph_war@yahoo.ca>
2022-04-06 11:18:54 -0700

    js react http requests, hacky sign in and default port

--- js_react/awesome-answer-client/src/App.js
+++ js_react/awesome-answer-client/src/App.js
@@ -4,14 +4,33 @@ import React, { Component } from 'react';
 import CurrentDateTime from './components/CurrentDateTime'
 // import QuestionShowPage from './components/QuestionShowPage'
 // import QuestionIndexPage from './components/QuestionIndexPage'
+import { Session } from './requests'
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      clocksCount: [1,5,9] // an array of something
+      clocksCount: [1,5,9], // an array of something
+      user: null
     }
   }
+
+  //Hacky Sign In
+  componentDidMount(){
+    Session.create(
+      {
+        email: "admin@user.com",
+        password: "123"
+      }
+    ).then(fetchedUser => {
+      this.setState((state) => {
+        return {
+          user: fetchedUser
+        }
+      })
+    })
+  }
+
   render(){
     return(
       <div>
---
+++ js_react/awesome-answer-client/src/requests.js
@@ -0,0 +1,56 @@
+const baseUrl = "http://localhost:3000/api/v1";
+
+export const Question = {
+    // fetch all the question from the rails server
+    index() {
+        return fetch(`${baseUrl}/questions`).then(res => res.json())
+    },
+    show(qid) {
+        return fetch(`${baseUrl}/questions/${qid}`).then(res => res.json())
+    },
+    create(params) {
+        return fetch(`${baseUrl}/questions`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(params),
+            credentials: "include"
+        }).then(res => res.json())
+    },
+    update(params, qid) {
+        return fetch(`${baseUrl}/questions/${qid}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(params),
+            credentials: "include"
+        }).then(res => res.json())
+    },
+    destroy(qid) {
+        return fetch(`${baseUrl}/questions/${qid}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: "include"
+        }).then(res => res.json())
+    }
+}
+
+export const Session = {
+    create(params) {
+        return fetch(`${baseUrl}/session`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(params),
+            credentials: "include"
+        }).then(res => {
+            return res.json()
+        })
+    }
+}
+
