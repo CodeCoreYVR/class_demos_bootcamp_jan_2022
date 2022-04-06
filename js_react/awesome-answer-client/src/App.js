925bdaff14dca7afbb9ee9503bf7b9941a980fb3
stephanie-warmenhoven <steph_war@yahoo.ca>
2022-04-05 17:59:23 -0700

    js react: set up CurrentDateTime component and reconstruct App.js

--- js_react/awesome-answer-client/src/App.js
+++ js_react/awesome-answer-client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import QuestionShowPage from './components/QuestionShowPage'
+import QuestionIndexPage from './components/QuestionIndexPage'
+import CurrentDateTime from './components/CurrentDateTime'
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      clocksCount: [1] //an array of something
+    }
+  }
+  render(){
+    return(
+      <CurrentDateTime />
+    )
+  }
 }
 
 export default App;
---
+++ js_react/awesome-answer-client/src/components/CurrentDateTime.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class CurrentDateTime extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          date: new Date()
+      }
+      console.log('1: constructor fired')
+  }
+
+  render() {
+      return (
+          <div>
+              CURRENT TIME: {this.state.date.toLocaleString()}
+          </div>
+      )
+  }
+}
+
+export default CurrentDateTime;
