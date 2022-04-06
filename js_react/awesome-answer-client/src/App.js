617b4520c701ff25090dd816fc83f1450e2c52d1
stephanie-warmenhoven <steph_war@yahoo.ca>
2022-04-06 09:07:32 -0700

    http requests and Hacky Sign In

--- js_react/awesome-answer-client/src/App.js
+++ js_react/awesome-answer-client/src/App.js
@@ -1,17 +1,35 @@
 import './App.css';
 import React, {Component} from 'react';
 
-import QuestionShowPage from './components/QuestionShowPage'
-import QuestionIndexPage from './components/QuestionIndexPage'
+// import QuestionShowPage from './components/QuestionShowPage'
+// import QuestionIndexPage from './components/QuestionIndexPage'
 import CurrentDateTime from './components/CurrentDateTime'
+import { Session } from './requests'
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      clocksCount: [5,9,8] //an array of something
+      clocksCount: [5,9,8], //an array of something
+      user: null
     }
   }
+
+  //Hacky sign in
+  componentDidMount(){
+    Session.create({
+      email: 'admin@user.com',
+      password: '123'
+    })
+    .then(user => {
+      this.setState((state) => {
+        return {
+          user: user
+        }
+      })
+    })
+  }
+
   render(){
     return(
       <div>
