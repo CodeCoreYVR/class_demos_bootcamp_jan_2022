c077ddee16dda9db851010fbdac7f581fa98db06
stephanie-warmenhoven <steph_war@yahoo.ca>
2022-04-08 12:07:17 -0700

    js react hooks refactor App.js

--- js_react/awesome-answer-client/src/App.js
+++ js_react/awesome-answer-client/src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CurrentDateTime from './components/CurrentDateTime'
 import QuestionShowPage from './components/QuestionShowPage';
 import QuestionIndexPage from './components/QuestionIndexPage';
@@ -15,32 +15,36 @@ import SignUpPage from './components/SignUpPage';
 import UseStateHook from './components/UseStateHook';
 import UseEffectHook from './components/UseEffectHook';
 
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      clocksCount: true, // an array of something
-      user: null
-    }
-  }
+export default function App() {
+  const [user, setUser] = useState(null);
+  const [clocksCount, setClocksCount] = useState(true)
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     clocksCount: true, // an array of something
+  //     user: null
+  //   }
+  // }
 
-  componentDidMount(){
-    this.getCurrentUser()
-  }
+  // componentDidMount(){
+  //   this.getCurrentUser()
+  // }
 
-  getCurrentUser = () => {
+  useEffect(() => {
+    getCurrentUser();
+  }, [])
+
+  const getCurrentUser = () => {
     return User.current().then(user => {
       //This is the safe navigation operator
       //similar to user && user.id (user&.id)
       if (user?.id){
-        this.setState(state => {
-          return { user }
-        })
+        setUser(user)
       }
     })
   }
 
-  onSignOut = () => { this.setState( { user: null })}
+  const onSignOut = () => { setUser(null) }
 
   //Anytime we want to render a component that requires some props,
   // and that component is being rendered by a Route component
@@ -49,25 +53,23 @@ class App extends Component {
   //with the props passed to it. "routeProps" represents all the routing props
   //Make sure to pass them to the component as well
 
-  render(){
     return(
       <BrowserRouter>
-        <NavBar currentUser={this.state.user} onSignOut={this.onSignOut} clocksCount={this.state.clocksCount}/>
+        <NavBar currentUser={user} onSignOut={onSignOut} clocksCount={clocksCount}/>
         <Switch>
           <Route exact path='/sign_in'
-          render={(routeProps) => <SignInPage {...routeProps } onSignIn={this.getCurrentUser} />}
+          render={(routeProps) => <SignInPage {...routeProps } onSignIn={getCurrentUser} />}
           />
           <Route exact path='/sign_up' 
-          render={(routeProps) => <SignUpPage {...routeProps} onSignUp={this.getCurrentUser} />}/>
+          render={(routeProps) => <SignUpPage {...routeProps} onSignUp={getCurrentUser} />}/>
           <Route exact path='/questions' component={QuestionIndexPage}/>
-          <AuthRoute isAuthenticated={!!this.state.user} exact path='/questions/new' component={NewQuestionPage}></AuthRoute>
+          <AuthRoute isAuthenticated={!!user} exact path='/questions/new' component={NewQuestionPage}></AuthRoute>
           <Route exact path='/questions/:id' component={QuestionShowPage} />
           <Route path='/use_state' component={UseStateHook} />
           <Route path='/use_effect' component={UseEffectHook} />
         </Switch>
       </BrowserRouter>
     )
-  }
 }
 
-export default App;
+// export default App;
