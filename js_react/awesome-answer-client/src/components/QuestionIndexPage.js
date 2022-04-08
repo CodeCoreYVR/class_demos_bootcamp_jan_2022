eb6c4cac8bd490cdacf8ed783184b20dd2dcf577
stephanie-warmenhoven <steph_war@yahoo.ca>
2022-04-08 11:49:41 -0700

    js react hooks refactor QuestionShowPage with useState and useEffect

--- js_react/awesome-answer-client/src/components/QuestionIndexPage.js
+++ js_react/awesome-answer-client/src/components/QuestionIndexPage.js
@@ -1,84 +1,76 @@
 import QuestionDetails from './QuestionDetails';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 // import questionIndexData from '../questionIndexData';
 import { Question } from '../requests';
 import { Link } from 'react-router-dom';
 
-class QuestionIndexPage extends Component {
-    constructor(props){
-        super(props);
-        this.state = { questions: []}
-    }
+export default function QuestionIndexPage() {
+    const [questions, setQuestions] = useState([])
 
-    componentDidMount(){
+    useEffect(() => {
         Question.index()
         .then((questionsData) => {
-            this.setState((state)=>{
-                return {
-                    questions: questionsData
-                }
-            })
+            setQuestions(questionsData)
         })
-    }
-    
-    deleteQuestion(id) {
-        console.log(id)
-        // we cant reset the value of the state in this way
-        // this.state.questions = this.state.questions.filter(q => q.id != id)
-        // you can only reset the value by using this.setState()
+    }, [])
 
-        this.setState({
-            questions: this.state.questions.filter(q => q.id != id)
-        })
-        // this.setState((state, props) => {
-        //     // sth here
-        //     return {
-        //         questions: this.state.questions.filter(q => q.id != id)
-        //     }
-        // })
-    }
-
-    render() {
-        return (
-            <>
-                <ul>
-                    {
-                        this.state.questions.map((e, i) => {
-                            return <li key={i}>{e.id} - <Link to={`/questions/${e.id}`}>{e.title}</Link> <button onClick={() => { this.deleteQuestion(e.id) }}>Delete</button> </li>
-                        })
-                    }
-                </ul>
-            </>
+    const deleteQuestion = (id) => {
+        setQuestions(
+            questions.filter(q => q.id !== id)
         )
     }
+
+    return(
+        <div>
+            { questions.map((e, i) => {
+                return (
+                    <h3 key={i}>{e.id} - <Link to={`/questions/${e.id}`}>{e.title}</Link> <button onClick={() => { deleteQuestion(e.id) }}>Delete</button> </h3>
+                )
+            })}
+        </div>
+    )
 }
 
-//#region functional component
-// const QuestionIndexPage = () => {
-//     const data = [
-//         { title: "title1", body: "body1", author: { full_name: "name1" }, view_count: 100, created_at: new Date() },
-//         { title: "title2", body: "body2", author: { full_name: "name2" }, view_count: 100, created_at: new Date() },
-//         { title: "title3", body: "body3", author: { full_name: "name3" }, view_count: 100, created_at: new Date() },
-//         { title: "title4", body: "body4", author: { full_name: "name4" }, view_count: 100, created_at: new Date() },
-//     ]
+// class QuestionIndexPage extends Component {
+//     constructor(props){
+//         super(props);
+//         this.state = { questions: []}
+//     }
 
-//     return (
-//         <div>
-//             {
-//                 data.map((e, i) => {
-//                     return <QuestionDetails
-//                         key={i}
-//                         title={e.title}
-//                         body={e.body}
-//                         author={e.author}
-//                         view_count={e.view_count}
-//                         created_at={e.created_at}
-//                     />
-//                 })
-//             }
-//         </div>
-//     )
+//     componentDidMount(){
+//         Question.index()
+//         .then((questionsData) => {
+//             this.setState((state)=>{
+//                 return {
+//                     questions: questionsData
+//                 }
+//             })
+//         })
+//     }
+    
+//     deleteQuestion(id) {
+//         console.log(id)
+//         this.setState({
+//             questions: this.state.questions.filter(q => q.id != id)
+//         })
+        
+//     }
+
+//     render() {
+//         return (
+//             <>
+//                 <ul>
+//                     {
+//                         this.state.questions.map((e, i) => {
+//                             return <li key={i}>{e.id} - <Link to={`/questions/${e.id}`}>{e.title}</Link> <button onClick={() => { this.deleteQuestion(e.id) }}>Delete</button> </li>
+//                         })
+//                     }
+//                 </ul>
+//             </>
+//         )
+//     }
 // }
-//#endregion
 
-export default QuestionIndexPage;
+
+
+// export default QuestionIndexPage;
--- js_react/awesome-answer-client/src/components/QuestionShowPage.js
+++ js_react/awesome-answer-client/src/components/QuestionShowPage.js
@@ -1,107 +1,109 @@
 import AnswerDetails from './AnswerDetails';
-import { Component } from 'react'
+import { useState, useEffect } from 'react'
 import QuestionDetails from './QuestionDetails';
 import AnswerList from './AnswerList'
 import '../App.css';
 // import questionData from '../questionData';
 import { Question } from '../requests';
 
-class QuestionShowPage extends Component {
-    // two ways to for declaring state
-    constructor(props) {
-        super(props);
-        this.state = { stateQuestion : {} }
-        // bind the key word this in the method
-        // so the key word `this` is always the class QuestionShowPage
-        this.delete = this.delete.bind(this);
-        // this.deleteTheAnswer = this.deleteTheAnswer.bind(this);
-    }
-    // state = {
-    //     question: questionData,
-    //     times: 1
-    // }
+export default function QuestionShowPage(props){
+    const [question, setQuestion] = useState({});
 
-    componentDidMount(){
-        // Question.show(7) //just hard code to fetch question 45 for now
-        Question.show(this.props.match.params.id) //not hard coded anymore - we have access to params and the method match through our router
-        .then((fetchedAPIquestion) => {
-            this.setState((state) => {
-                return {
-                    stateQuestion : fetchedAPIquestion
-                }
-            })
+    useEffect(() => {
+        Question.show(props.match.params.id)
+        .then((fetchedAPIQuestion) => {
+            setQuestion(fetchedAPIQuestion)
         })
-    }
+    }, [])
 
-    componentDidUpdate(){
-        console.log('the state has been updated with the fetched data')
-    }
-
-    delete() {
-        this.setState({
-            question: null
-        })
-    }
-    deleteTheAnswer(id) {
-        this.setState({
-            question: {
-                ...this.state.question,
-                answers: this.state.answers.filter(a => a.id !== id),
+    const deleteAnswer = id => {
+        const { answers, ...rest } = question;
+        setQuestion(
+            {
+                answers: answers.filter(a => a.id !== id),
+                ...rest
             }
-        })
-    }
-    render() {
-        const question = this.state.stateQuestion
-        return (
-            // styles are in camelcased
-            // for using a separate stylesheet:
-            // 1. import it
-            // 2. put the name inside className attribute
-            <div style={{ marginLeft: "2rem" }} className="App">
-                <QuestionDetails
-                    title={question.title}
-                    body={question.body}
-                    author={question.author}
-                    created_at={question.created_at}
-                    view_count={question.view_count}
-                // except the strings, all the other values should be inside the {}.
-                // e.g. view_count={100} is_admin={false} created_at={new Date()} author={{full_name: "Admin User"}}
-                />
-                {/* this key prop should be unique in the array of the elements */}
-                {/* {
-                [
-                    <AnswerDetails key={0} body="red" author_name="AAA" created_at="2022-04-03" />,
-                    <AnswerDetails key={1} body="yellow" author_name="BBB" created_at="2022-04-02" />,
-                    <AnswerDetails key={2} body="blue" author_name="CCC" created_at="2022-04-04" />,
-                    <AnswerDetails key={3} body="green" author_name="DDD" created_at="2022-04-09" />,
-                ]
-            } */}
-                <button onClick={this.delete}>Delete The Question</button>
-                <AnswerList list={question.answers} deleteTheAnswer={(id) => this.deleteTheAnswer(id)} />
-            </div>
         )
     }
+    const { title, body, author, view_count, created_at } = question;
+
+    return (
+        <div>
+            <QuestionDetails 
+            title={title}
+            body={body}
+            author={author}
+            created_at={created_at}
+            view_count={view_count}
+            />
+             <AnswerList list={question.answers} deleteTheAnswer={(id) => deleteAnswer(id)} />
+        </div>
+    )
 }
 
-//#region functional component
-// const QuestionShowPage = () => {
-//     return (
-//         // styles are in camelcased
-//         // for using a separate stylesheet:
-//         // 1. import it
-//         // 2. put the name inside className attribute
-//         <div style={{ marginLeft: "2rem" }} className="App">
-//             <QuestionDetails
-//                 title="What is your favourite color"
-//                 body="Red, green, blue, etc"
-//                 author={{ full_name: "Admin User" }}
-//                 created_at={new Date()}
-//                 view_count={100}
-//             // except the strings, all the other values should be inside the {}.
-//             // e.g. view_count={100} is_admin={false} created_at={new Date()} author={{full_name: "Admin User"}}
-//             />
-//             {/* this key prop should be unique in the array of the elements */}
-//             {/* {
+// class QuestionShowPage extends Component {
+//     // two ways to for declaring state
+//     constructor(props) {
+//         super(props);
+//         this.state = { stateQuestion : {} }
+//         // bind the key word this in the method
+//         // so the key word `this` is always the class QuestionShowPage
+//         this.delete = this.delete.bind(this);
+//         // this.deleteTheAnswer = this.deleteTheAnswer.bind(this);
+//     }
+//     // state = {
+//     //     question: questionData,
+//     //     times: 1
+//     // }
+
+//     componentDidMount(){
+//         // Question.show(7) //just hard code to fetch question 45 for now
+//         Question.show(this.props.match.params.id) //not hard coded anymore - we have access to params and the method match through our router
+//         .then((fetchedAPIquestion) => {
+//             this.setState((state) => {
+//                 return {
+//                     stateQuestion : fetchedAPIquestion
+//                 }
+//             })
+//         })
+//     }
+
+//     componentDidUpdate(){
+//         console.log('the state has been updated with the fetched data')
+//     }
+
+//     delete() {
+//         this.setState({
+//             question: null
+//         })
+//     }
+//     deleteTheAnswer(id) {
+//         this.setState({
+//             question: {
+//                 ...this.state.question,
+//                 answers: this.state.answers.filter(a => a.id !== id),
+//             }
+//         })
+//     }
+//     render() {
+//         const question = this.state.stateQuestion
+//         return (
+//             // styles are in camelcased
+//             // for using a separate stylesheet:
+//             // 1. import it
+//             // 2. put the name inside className attribute
+//             <div style={{ marginLeft: "2rem" }} className="App">
+//                 <QuestionDetails
+//                     title={question.title}
+//                     body={question.body}
+//                     author={question.author}
+//                     created_at={question.created_at}
+//                     view_count={question.view_count}
+//                 // except the strings, all the other values should be inside the {}.
+//                 // e.g. view_count={100} is_admin={false} created_at={new Date()} author={{full_name: "Admin User"}}
+//                 />
+//                 {/* this key prop should be unique in the array of the elements */}
+//                 {/* {
 //                 [
 //                     <AnswerDetails key={0} body="red" author_name="AAA" created_at="2022-04-03" />,
 //                     <AnswerDetails key={1} body="yellow" author_name="BBB" created_at="2022-04-02" />,
@@ -109,16 +111,13 @@ class QuestionShowPage extends Component {
 //                     <AnswerDetails key={3} body="green" author_name="DDD" created_at="2022-04-09" />,
 //                 ]
 //             } */}
-//             <AnswerList list={
-//                 [
-//                     { body: "red", author_name: "AAA", created_at: "2022-04-05" },
-//                     { body: "yellow", author_name: "BBB", created_at: "2022-04-05" },
-//                     { body: "blue", author_name: "CCC", created_at: "2022-04-05" }
-//                 ]
-//             } />
-//         </div>
-//     )
+//                 <button onClick={this.delete}>Delete The Question</button>
+//                 <AnswerList list={question.answers} deleteTheAnswer={(id) => this.deleteTheAnswer(id)} />
+//             </div>
+//         )
+//     }
 // }
-//#endregion
 
-export default QuestionShowPage;
+
+
+// export default QuestionShowPage;
