ef1216c2de131ff244227c6d0d53d2657e48eb2b
stephanie-warmenhoven <steph_war@yahoo.ca>
2022-04-08 10:06:50 -0700

    js react hooks refactor SignInPage with useState

--- js_react/awesome-answer-client/src/components/SignInPage.js
+++ js_react/awesome-answer-client/src/components/SignInPage.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Session} from '../requests'
 
 function SignInPage(props){
     const { onSignIn } = props;
 
+    // const [inputText, setInputText] = useState('') 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     function handleSubmit(event){
         event.preventDefault();
-        const { currentTarget } = event;
-        const formData = new FormData(currentTarget)
+        // const { currentTarget } = event;
+        // const formData = new FormData(currentTarget)
+        // const params = {
+        //     email: formData.get('email'),
+        //     password: formData.get('password')
+        // }
+
         const params = {
-            email: formData.get('email'),
-            password: formData.get('password')
+            email: email,
+            password: password
         }
         Session.create(params).then(data => {
             if (data.id){
@@ -29,11 +38,13 @@ function SignInPage(props){
             <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="email">Email</label>
-                    <input type="text" name="email" id="email" />
+                    <input type="text" name="email" id="email" onChange={event => {
+                        setEmail(event.currentTarget.value); }}/>
                 </div>
                 <div>
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" />
+                    <input type="password" name="password" id="password" onChange={event => {
+                        setPassword(event.currentTarget.value); }}/>
                 </div>
                 <input type="submit" value="Sign In" />
             </form>
