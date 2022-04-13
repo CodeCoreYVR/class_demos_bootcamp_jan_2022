4fca00f2ec68e6129635c3029663f8183238dcf5
stephanie-warmenhoven <steph_war@yahoo.ca>
2022-04-13 11:33:00 -0700

    rails GitHub oauth with omniauth gem

--- rails_project/awesome_answers/app/models/user.rb
+++ rails_project/awesome_answers/app/models/user.rb
@@ -32,4 +32,32 @@ class User < ApplicationRecord
     def full_name
         self.first_name + " " + self.last_name
     end
+
+    #OmniAuth User setup
+    VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+    validates :email, presence: true, uniqueness: true, format: VALID_EMAIL_REGEX, unless: :from_oath?
+
+    def from_oath?
+        uid.present? && provider.present?
+    end
+
+    def self.create_from_oauth(oauth_data)
+        name = oauth_data["info"]["name"]&.split || oauth_data["info"]["nickname"]
+        self.create(
+            first_name: name[0],
+            last_name: name[1],
+            uid: oauth_data["uid"],
+            provider: oauth_data["provider"],
+            oauth_raw_data: oauth_data,
+            password: SecureRandom.hex(32)
+        )
+    end
+
+    def self.find_by_oauth(oauth_data)
+        self.find_by( 
+            uid: oauth_data["uid"],
+            provider: oauth_data["provider"]
+        )
+    end
+
 end
