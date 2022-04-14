12fcb40d35595ec9134f61a3a2110cf55a71ced5
stephanie-warmenhoven <steph_war@yahoo.ca>
2022-04-14 15:08:44 -0700

    js react-native moved Header to its own component

--- react_native/pokedex/App.js
+++ react_native/pokedex/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header'
 
 //To create this app:
 // $ npm i -g expo-cli
@@ -17,9 +18,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.h1}>Pokédex</Text>
-      </View>
+      <Header/>
       <View style={styles.body}>
         <Text>Hello world</Text>
       </View>
@@ -34,19 +33,7 @@ const styles = StyleSheet.create({
     alignItems: 'stretch',
     justifyContent: 'center'
   },
-  header: {
-    flex: 1,
-    backgroundColor: 'red',
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: 25,
-    borderBottomWidth: 15
-  },
   body: {
     flex: 5
-  },
-  h1: {
-    fontSize: 40
   }
 });
