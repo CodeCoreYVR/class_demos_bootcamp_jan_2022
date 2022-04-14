dc6c1935d906b0fa12c9031499bd288b5d0d1b08
stephanie-warmenhoven <steph_war@yahoo.ca>
2022-04-14 15:36:26 -0700

    js react-native Add react-navigation/native and /stack

--- react_native/pokedex/App.js
+++ react_native/pokedex/App.js
@@ -3,8 +3,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
 import PokemonList from './components/PokemonList';
+import { NavigationContainer } from '@react-navigation/native'; //this is necessary for navigation
+import { createStackNavigator } from '@react-navigation/stack'; //this is necessary for navigation
 
-//To create this app:
+// -----To create this app:---->
 // $ npm i -g expo-cli
 // $ expo init pokedex
 // - choose the blank template
@@ -15,6 +17,30 @@ import PokemonList from './components/PokemonList';
 // $ expo start
 
 //If you open up Expo Devtools, you could open a simulator and see if the app works - it should display “Open up App.js to start working on your app!”
+
+// -----Navigation---->
+// react-native does not work with urls, so therefore navigation works differently.  You need to install dependencies:
+// https://reactnavigation.org/docs/nesting-navigators
+// https://reactnavigation.org/docs/getting-started/
+
+// $ npm install @react-navigation/native
+// $ npm install @react-navigation/stack
+// $ expo install react-native-screens react-native-safe-area-context
+
+// If the installation of these dependencies give you the following error:
+
+// iOS Bundling failed 8281ms
+// Unable to resolve module react-native-gesture-handler
+
+// Try fixing it with this:
+
+// $ npm uninstall react-native-gesture-handler --save 
+// $ npm install react-native-gesture-handler --save
+
+
+
+//create stack object
+const Stack = createStackNavigator();
 
 const POKEMON_API_ENDPOINT = 'https://pokeapi.co/api/v2/';
 const FIRST_100_POKEMON_QUERY = 'pokemon?limit=100';
@@ -32,13 +58,26 @@ export default function App() {
   }, [])
 
   return (
-    <View style={styles.container}>
+    <NavigationContainer style={styles.container}>
       <Header/>
       <View style={styles.body}>
-        <PokemonList list={pokemonList}/>
+        <Stack.Navigator
+          screenOptions={
+            {
+              headerShown: false
+            }
+          }
+          >
+          <Stack.Screen
+          name="Pokemon List"
+          children={()=>{
+            return <PokemonList list={pokemonList}/>
+          }}
+          />
+        </Stack.Navigator>
       </View>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
 
