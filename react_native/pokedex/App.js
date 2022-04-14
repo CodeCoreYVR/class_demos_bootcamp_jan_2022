1f3c1d4f5021a40114d5016961a2b21e4947559f
stephanie-warmenhoven <steph_war@yahoo.ca>
2022-04-14 15:12:55 -0700

    js react-native Fetch pokemon payload with React Hooks and fetch request to pokemon.io api

--- react_native/pokedex/App.js
+++ react_native/pokedex/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header'
@@ -14,8 +15,20 @@ import Header from './components/Header'
 
 //If you open up Expo Devtools, you could open a simulator and see if the app works - it should display “Open up App.js to start working on your app!”
 
+const POKEMON_API_ENDPOINT = 'https://pokeapi.co/api/v2/';
+const FIRST_100_POKEMON_QUERY = 'pokemon?limit=100';
 
 export default function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(()=> {
+    fetch(`${POKEMON_API_ENDPOINT}${FIRST_100_POKEMON_QUERY}`)
+    .then(res => res.json())
+    .then(payload => {
+      console.log(payload);
+    })
+  }, [])
+  
   return (
     <View style={styles.container}>
       <Header/>
